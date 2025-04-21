00de945508a89b9cc6d8e3d086aad671b6f65439
phuongquang <phuongquang4900@gmail.com>
2025-04-21 17:50:25 +0700

    Uplift state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import { useState } from "react";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 3, description: "Charger", quantity: 1, packed: false },
-];
-
 export default function App() {
+
+  const [items, setItems] = useState([]);
+
+  function handleAddItem(item) {
+    setItems(items => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems(items => [...items].filter(item => item.id !== id));
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <Packinglist />
+      <Form onAddItem={handleAddItem} />
+      <Packinglist items={items} onDeleteItem={handleDeleteItem}/>
       <Stats />
     </div>
   );
@@ -21,7 +26,7 @@ function Logo() {
   return <h1>🏖️ Far Away ✈️</h1>;
 }
 
-function Form() {
+function Form({onAddItem}) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
 
@@ -31,6 +36,10 @@ function Form() {
     if (!description) return;
 
     const newItem = { description, quantity, packed: false, id: Date.now() };
+
+    onAddItem(newItem);
+
+    console.log('newItem: ', newItem);
 
     setDescription("");
     setQuantity(1);
@@ -61,25 +70,25 @@ function Form() {
   );
 }
 
-function Packinglist() {
+function Packinglist({items, onDeleteItem}) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
-          <Item key={item.id} item={item} />
+        {items.map((item) => (
+          <Item key={item.id} item={item} onDeleteItem={onDeleteItem} />
         ))}
       </ul>
     </div>
   );
 }
 
-function Item({ item }) {
+function Item({ item, onDeleteItem }) {
   return (
     <li>
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
-      <button>❌</button>
+      <button onClick={()=>onDeleteItem(item.id)}>❌</button>
     </li>
   );
 }
