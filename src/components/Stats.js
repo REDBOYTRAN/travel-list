14e6b968164a587eced80a2e4f2f8fa758af27a7
phuongquang <phuongquang4900@gmail.com>
2025-04-23 15:13:20 +0700

    Moving components into separate files

---
+++ src/components/Stats.js
@@ -0,0 +1,16 @@
+export default function Stats({ items }) {
+
+  if (items.length === 0) return (<p className="stats">
+    <em>Start adding some items to your packing list 🛬</em>
+  </p>);
+
+  const numItems = items.length;
+  const numPacked = items.filter(item => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
+  return (
+    <footer className="stats">
+      <em>{percentage !== 100 ? `🧳 You have ${numItems} items on your list, and you already packed ${numPacked} (${percentage}%)` : "You got everything! Ready to go 🧳"}</em>
+    </footer>
+  );
+}
